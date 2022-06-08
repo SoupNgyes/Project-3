3bf7a2261a62a80b94557a875b12779fed2e1e36
fruitcake <80382564+SoupNgyes@users.noreply.github.com>
2022-06-08 13:30:50 +0000

    warm up

---
+++ main.py
@@ -0,0 +1,12 @@
+play = True
+
+while play == True:
+  bob = int(input("Gurunyuu: "))
+  if bob > 0:
+      print("This number is positive")
+  elif bob < 0:
+    print("this number is negative")
+  elif bob == 0: 
+    print("this number is zero")
+
+  
