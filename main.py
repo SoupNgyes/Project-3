ca46a8737178b39ca3f0a1f5261425400f04283b
fruitcake <80382564+SoupNgyes@users.noreply.github.com>
2022-06-08 15:28:25 +0000

    before square

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
 import turtle
 
-turtle.bgcolor("lightgreen")
+turtle.bgcolor("white")
 turtle.setup (600, 600)
 turtle.shape ("turtle")
 
 screen = turtle.Screen()
-bob = turtle.Turtle()
+kaia = turtle.Turtle()
 
-#color = input("Give me a color: ")
-#length = input("Give me a length: ")
 
-def star():
-    print("star")
-  def triangle():
-    print("triangle")
-  def circle():
-    print("circle")
 
+def star(color, length):
+  kaia.fillcolor(color)
+  kaia.begin_fill()
+  
+  corners = 0
+  while corners < 5:
+    kaia.forward(length)
+    kaia.right(144)
+    corners +=1
+  
+  kaia.end_fill()
+
+
+def triangle(color, length):
+  kaia.fillcolor(color)
+  kaia.begin_fill()
+  
+  corners = 0
+  while corners < 3:
+    kaia.forward(length)
+    kaia.right(120)
+    corners +=1
+  
+  kaia.end_fill()
+
+def circle(color, length):
+  kaia.fillcolor(color)
+  kaia.begin_fill()
+  
+  kaia.circle(length)
+
+  kaia.end_fill()
+
+
+
+  
+
+def square(color,length):
 
 shape = input("Give me a shape: ")
+color = input("Give me a color: ")
+length = int(input("Give me a length: "))
 
 if shape == "star":
-  star()
+  star(color, length)
 elif shape == "triangle":
-  triangle()
+  triangle(color, length)
 elif shape == "circle":
-  circle()
+  circle(color, length)
+elif shape == "square":
+  square(color, length)
